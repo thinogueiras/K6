9abae375f395530421349d6515f4e25a4200d9c7
Thiago Nogueira dos Santos <thinogueiras@gmail.com>
2023-02-06 12:43:59 -0300

    New configurations on signup.js

--- tests/signup.js
+++ tests/signup.js
@@ -1,18 +1,27 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 
-import uudi from './libs/uuid.js';
+import uuid from './libs/uuid.js';
+
+export const options = {
+    vus: 10,
+    duration: '30s',
+    thresholds: {
+        http_req_duration: ['p(95)<2000'], // 95% das req's devem responder em até 2s
+        http_req_failed: ['rate<0.01'] // 1% das req's podem ocorrer erro
+    }
+}
 
 export default function () {
     const url = 'http://localhost:3333/signup';
 
-    const payload = JSON.stringify(
-        { email: `${uudi.v4().substring(24)}@qa.qacademy.com.br`, password: '123456' },
-    );
-
     const headers = {
         headers: { 'Content-Type': 'application/json' },
     };
+
+    const payload = JSON.stringify(
+        { email: `${uuid.v4().substring(24)}@qa.qacademy.com.br`, password: '123456' },
+    );
 
     const res = http.post(url, payload, headers);
 
